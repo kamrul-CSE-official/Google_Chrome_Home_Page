5b6375ccb77fd916bfb61e05c8b89d74d461b077
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2023-06-14 02:41:06 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
-import React from "react";
-import Search from "./Components/SearchComponents/Search";
+"use client";
+import ImportantLink from "@/Components/ImportantLink/ImportantLink";
+import Search from "../Components/SearchComponents/Search";
 
 const Page = () => {
   return (
     <div className="h-screen w-full p-4">
-      <Search />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
-        laudantium rerum consequuntur eos! Suscipit fuga inventore molestiae,
-        sed quos labore assumenda sunt sit corrupti cum tenetur quidem quaerat
-        autem. Aperiam!
-      </p>
+      <div>
+        <Search />
+      </div>
+
+      <div>
+        <ImportantLink />
+      </div>
     </div>
   );
 };
